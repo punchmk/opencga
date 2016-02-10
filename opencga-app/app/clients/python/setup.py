2c0871cac2436c1571d72d144e89bb860b1d7bf0
Anand Kumria <akumria@acm.org>
2016-02-10 17:09:18 +0000

    Factor our BASE_DIR to make this a bit more readable

--- opencga-app/app/clients/python/setup.py
+++ opencga-app/app/clients/python/setup.py
@@ -2,15 +2,25 @@ from distutils.core import setup
 import uuid
 from pip.req import parse_requirements
 import os
-install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt"), session=uuid.uuid1())
+
+
+BASE_DIR = os.path.dirname(__file__)
+
+install_reqs = parse_requirements(os.path.join(BASE_DIR, "requirements.txt"), session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
-print os.path.join(os.path.dirname(__file__), 'pyCGA')
+
+print os.path.join(BASE_DIR, 'pyCGA')
+
 setup(
     name='pyCGA',
     packages=['pyCGA', 'pyCGA.Utils'],
     version='0.1.2',
-    scripts=[os.path.join(os.path.dirname(__file__),'pyCGA/Scripts/pyCGA'), os.path.join(os.path.dirname(__file__),'pyCGA/Scripts/pyCGAIdConverter'),
-             os.path.join(os.path.dirname(__file__),'pyCGA/Scripts/pyCGAVariantFetcher'), os.path.join(os.path.dirname(__file__),'pyCGA/Scripts/pyCGALogin')],
+    scripts=[
+        os.path.join(BASE_DIR, 'pyCGA/Scripts/pyCGA'),
+        os.path.join(BASE_DIR, 'pyCGA/Scripts/pyCGAIdConverter'),
+        os.path.join(BASE_DIR, 'pyCGA/Scripts/pyCGAVariantFetcher'),
+        os.path.join(BASE_DIR, 'pyCGA/Scripts/pyCGALogin')
+    ],
     url='',
     license='',
     author='antonio',
