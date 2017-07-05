cb4fc237044b16006e4d98a2eb3090b15969897f
pfurio <pedrofurio@gmail.com>
2017-07-05 17:18:40 +0100

    igv: Add support for GRCh37 and GRCh38 assemblies using the configuration file. Upgrade IGVjs version to 1.0.8.

--- opencga-web/src/config.js
+++ opencga-web/src/config.js
@@ -1,6 +1,10 @@
 var opencga = {
-    host: "bio-prod-opencga-haproxy-01.gel.zone/opencga",
+    host: "bioinfodev.hpc.cam.ac.uk/opencga-test",
     version: "v1",
+    assemblies: {
+        study1: "GRCh37"
+        // study1: "GRCh38"
+    },
     cookiePrefix: "gel-browser",
     expiration: 30  // Minutes passed to expire the session
 };
