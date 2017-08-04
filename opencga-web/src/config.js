cdfd958db64a2a2ca85f92898c44ec51bafb907c
pfurio <pedrofurio@gmail.com>
2017-08-04 13:19:11 +0100

    igv: Add different studies and assemblies to configuration file.

--- opencga-web/src/config.js
+++ opencga-web/src/config.js
@@ -2,7 +2,15 @@ var opencga = {
     host: "bio-prod-opencgainternal-haproxy-01.gel.zone/opencga",
     version: "v1",
     assemblies: {
-        1000000024: "GRCh37"
+        1000000024: "GRCh37", //RD37
+        1000000026: "GRCh37", //CG37
+        1000000028: "GRCh37", //UN37
+        1000000030: "GRCh37", //CS37
+        1000000032: "GRCh38", //RD38
+        1000000034: "GRCh38", //CG38
+        1000000036: "GRCh38", //UN38
+        1000000038: "GRCh38", //CS38
+        1000000041: "GRCh38"  //CS380317
     },
     filters: {
         // fixed filters that will be sent as query params to the query when searching for files
