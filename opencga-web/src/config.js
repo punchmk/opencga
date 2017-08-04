46c4142302f08ca07d103ff422da1d52cdfb521e
pfurio <pedrofurio@gmail.com>
2017-08-04 10:08:09 +0100

    igv: Preselect vcf files based on a preselection array of strings (SV, CNV, duprem...)

--- opencga-web/src/config.js
+++ opencga-web/src/config.js
@@ -8,7 +8,11 @@ var opencga = {
         // fixed filters that will be sent as query params to the query when searching for files
         query: {
             // attributes: "gelStatus=READY"
-        }
+        },
+        // list containing some strings. Only the files containing any of this strings in the file name will be preselected in the filter
+        // RD: only the vcfs that have SV, CNV, duprem.atomic.left.split.vcf.gz
+        // Cancer: SV, somatic.duprem.left.split.vcf
+        preselection: ["SV", "CNV", "duprem.atomic.left.split.vcf.gz", "somatic.duprem.left.split.vcf"]
     },
     cookiePrefix: "gel-browser",
     expiration: 30  // Minutes passed to expire the session
